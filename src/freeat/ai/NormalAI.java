0debddbb8f06ae579a53c63fbf9552505d0bbf3b
Otso <otso.nuortimo@gmail.com>
2014-12-21 15:37:22 +0200

    Added plane routes.

--- src/freeat/ai/NormalAI.java
+++ src/freeat/ai/NormalAI.java
@@ -24,31 +24,31 @@ public class NormalAI extends AI {
 
     @Override
     public void act(Player player) {
-        int dice = (int) (Math.random() * 6 + 1);
-        for (int i = 0; i < dice; i++) {
-            //System.out.println("balance: " + player.getCashBalance());
-            Node current = player.getCurrentNode();
-            if (player.isStayInCity()) {
-                player.tryToWinToken();
-                return;
-            }
-            int targetID = (int) (Math.random() * current.getConnections().size());
-            int target = current.getConnections().get(targetID);
-            Node targetNode = locations.get(target);
-            if (!targetNode.getConnections().isEmpty()) {
-                player.moveTo(target);
-                if (targetNode.hasTreasure()) {
-                    if (player.getCashBalance() >= 100) {
-                        player.buyToken();
-
-                    } else {
-                        player.stayInCity();
-                        return;
-                    }
-                }
-
-            }
-        }
+//        int dice = (int) (Math.random() * 6 + 1);
+//        for (int i = 0; i < dice; i++) {
+//            //System.out.println("balance: " + player.getCashBalance());
+//            Node current = player.getCurrentNode();
+//            if (player.isStayInCity()) {
+//                player.tryToWinToken();
+//                return;
+//            }
+//            int targetID = (int) (Math.random() * current.getConnections().size());
+//            int target = current.getConnections().get(targetID);
+//            Node targetNode = locations.get(target);
+//            if (!targetNode.getConnections().isEmpty()) {
+//                player.moveTo(target);
+//                if (targetNode.hasTreasure()) {
+//                    if (player.getCashBalance() >= 100) {
+//                        player.buyToken();
+//
+//                    } else {
+//                        player.stayInCity();
+//                        return;
+//                    }
+//                }
+//
+//            }
+//        }
     }
     
     @Override
