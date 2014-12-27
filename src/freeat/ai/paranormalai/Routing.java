c2cff12e0f06f15edac4e562941e3691cf689ca8
Antti Nuortimo <antti.nuortimo@gmail.com>
2014-12-28 00:12:51 +0200

    Alpha version of Paranormal AI.
    Crashes with null pointer exception.
    
    	new file:   src/freeat/ai/ParanormalAI.java
    	new file:   src/freeat/ai/paranormalai/FlightPath.java
    	new file:   src/freeat/ai/paranormalai/ParanormalNode.java
    	new file:   src/freeat/ai/paranormalai/Routing.java

---
+++ src/freeat/ai/paranormalai/Routing.java
@@ -0,0 +1,18 @@
+// Routing class for ParanormalAI.
+
+package freeat.ai.paranormalai;
+
+import java.util.ArrayList;
+import freeat.Node;
+
+public class Routing
+{
+    public ArrayList<ParanormalNode> routingArrayList;
+    public int nRoadsOrFlights;
+    
+    public Routing(ArrayList<ParanormalNode> routingArrayList)
+    {
+        this.routingArrayList = routingArrayList;
+        this.nRoadsOrFlights = routingArrayList.size();
+    }
+}
