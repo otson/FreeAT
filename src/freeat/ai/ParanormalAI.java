f4a00c8cf35d280eb6378cb60bb8feb6ab1a30ae
Antti Nuortimo <antti.nuortimo@gmail.com>
2015-01-01 19:16:06 +0200

    Removed `System.out.println` method call.
    	modified:   src/freeat/ai/ParanormalAI.java

--- src/freeat/ai/ParanormalAI.java
+++ src/freeat/ai/ParanormalAI.java
@@ -372,7 +372,6 @@ public class ParanormalAI extends AI
                 }
             }
             c.endTurn();
-            System.out.println("End of while loop");
         }
 
         updateZeitgebers();
