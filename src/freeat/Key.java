92503cd811cf2a5f58c286ca03443dfcbb539059
Otso <otso.nuortimo@gmail.com>
2015-01-04 23:39:15 +0200

    Added Key class. Will be used as a the key in distance/price to get Route Hashmap.

---
+++ src/freeat/Key.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package freeat;
+
+/**
+ *
+ * @author otso
+ */
+public class Key
+{
+    private final int distance;
+    private final int price;
+
+    public Key(int distance, int price)
+    {
+        this.distance = distance;
+        this.price = price;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int hash = 5;
+        hash = 61 * hash + this.distance;
+        hash = 61 * hash + this.price;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (obj == null)
+        {
+            return false;
+        }
+        if (getClass() != obj.getClass())
+        {
+            return false;
+        }
+        final Key other = (Key) obj;
+        return true;
+    }
+    
+    
+}
