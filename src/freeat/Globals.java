bdde473ff0c6a957a877cacbb2b6b4c8d935e1a8
Otso <otso.nuortimo@gmail.com>
2015-01-05 10:38:35 +0200

    Added max_dice_value to Globals.

--- src/freeat/Globals.java
+++ src/freeat/Globals.java
@@ -11,9 +11,11 @@ package freeat;
  */
 public class Globals
 {
+
     // player variables
+
     public static final int START_CASH = 3;
-    
+
     // game variables
     public static final int MAXIMUM_ROUNDS = 200;
     public static final int DISPLAY_FRAME_RATE = 60;
@@ -43,11 +45,15 @@ public class Globals
 
     // treasure buying price
     public static final int TREASURE_BUYING_PRICE = 1;
-    
+
     // treasure opening with dice minimum value
     public static final int DICE_VALUE_TO_OPEN_TOKEN = (int) (DICE_SIZE * DICE_COUNT * 0.75);
 
     // Free sea movement amount
     public static final int FREE_SEA_MOVEMENT_LENGTH = 1;
+
+    // Values based on set Global values, do not modify
+    // Dice
+    public static final int MAX_DICE_VALUE = DICE_COUNT * DICE_SIZE;
 
 }
